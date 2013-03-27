2759912a3a88a17d507fa125f1beebfaf0213e51
dhenton <dhenton@dhenton>
2013-03-27 11:48:57 -0500

    adding code for hospital provider hierarchical data

--- neo4jSandbox/src/test/java/com/dhenton9000/neo4j/starter/neo4j/sandbox/inventory/HospitalTests.java
+++ neo4jSandbox/src/test/java/com/dhenton9000/neo4j/starter/neo4j/sandbox/inventory/HospitalTests.java
@@ -10,6 +10,9 @@ import org.junit.AfterClass;
 import static org.junit.Assert.*;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.neo4j.graphdb.Node;
+import org.neo4j.graphdb.index.Index;
+import org.neo4j.graphdb.index.IndexHits;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -20,7 +23,8 @@ import org.slf4j.LoggerFactory;
 public class HospitalTests extends BaseNeo4jTest {
 
     private final Logger logger = LoggerFactory.getLogger(HospitalTests.class);
-
+    private static final int STATE_COUNT = 47;
+    
     @BeforeClass
     public static void beforeClass() {
         prepareEmbeddedDatabase(DB_LOCATION);
@@ -33,9 +37,32 @@ public class HospitalTests extends BaseNeo4jTest {
     
     
     @Test
-    public void testThings()
+    public void testStateCount()
     {
-        assertTrue(true);
+        Index<Node> indexTypes = staticgraphDb.index().forNodes(TYPE_INDEX);
+        IndexHits<Node> states = 
+                indexTypes.get(NODE_TYPE.TYPE.toString(), 
+                NODE_TYPE.STATE_DIVISIONS.toString());
+        assertEquals(STATE_COUNT,states.size());
+         
     }
+    
+    @Test
+    public void testGetDivision()
+    {
+        String nodeName = "D012";
+        Node dItem =   getDivisionNode(nodeName); 
+        assertNotNull(dItem);
+        assertEquals(nodeName,dItem.getProperty(DIVISION_DISPLAY_PROPERTY));
+    }
+
+    private Node getDivisionNode(String nodeName) {
+        Index<Node> indexDivisionsDisplay = 
+                staticgraphDb.index().forNodes(DIVISION_DISPLAY_INDEX);
+        Node dItem = 
+                indexDivisionsDisplay.get(DIVISION_DISPLAY_PROPERTY, nodeName).getSingle();
+        return dItem;
+    }
+    
     
 }
