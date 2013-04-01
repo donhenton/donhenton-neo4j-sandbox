3bb2924f76f9cc656479bb0b6d7654a0eddaa718
dhenton <dhenton@dhenton>
2013-04-01 10:49:38 -0500

    working on JSON representation

--- neo4jSandbox/src/test/java/com/dhenton9000/neo4j/starter/neo4j/sandbox/hospital/JsonHospitalTests.java
+++ neo4jSandbox/src/test/java/com/dhenton9000/neo4j/starter/neo4j/sandbox/hospital/JsonHospitalTests.java
@@ -8,6 +8,7 @@ import com.dhenton9000.neo4j.starter.neo4j.sandbox.BaseNeo4jTest;
 import static com.dhenton9000.neo4j.starter.neo4j.sandbox.hospital.HospitalDbMaker.*;
 import com.dhenton9000.neo4j.starter.neo4j.sandbox.hospital.HospitalDbMaker.NODE_TYPE;
 import com.dhenton9000.neo4j.starter.neo4j.sandbox.hospital.json.Division;
+import com.dhenton9000.neo4j.starter.neo4j.sandbox.hospital.json.JSONHospitalService;
 import java.io.IOException;
 import org.codehaus.jackson.map.ObjectMapper;
 import org.junit.AfterClass;
@@ -28,12 +29,13 @@ import org.slf4j.LoggerFactory;
 public class JsonHospitalTests extends BaseNeo4jTest {
 
     private final Logger logger = LoggerFactory.getLogger(HospitalTests.class);
-    private ObjectMapper mapper = new ObjectMapper();
+    private static JSONHospitalService jsonService = new JSONHospitalService();
     //  private static String tString = "";
 
     @BeforeClass
     public static void beforeClass() {
         prepareEmbeddedDatabase(DB_LOCATION);
+        jsonService.setNeo4jDb(staticgraphDb);
         // tString = "";
     }
 
@@ -46,12 +48,12 @@ public class JsonHospitalTests extends BaseNeo4jTest {
     public void testBuildJSON() throws IOException {
 
         Division root = new Division();
-        Node dItem = getDivisionNode(PROGRAM_NAME);
+        Node dItem = jsonService.getDivisionNode(PROGRAM_NAME);
         String nextItem = (String) dItem.getProperty(DIVISION_DISPLAY_PROPERTY);
         root.setLabel(nextItem);
         root.setId(dItem.getId());
-        buildJSON(dItem, root);
-        String temp = mapper.defaultPrettyPrintingWriter().writeValueAsString(root);
+        jsonService.buildJSON(dItem, root);
+       
        // logger.info("\n" + temp);
 
 
@@ -62,15 +64,13 @@ public class JsonHospitalTests extends BaseNeo4jTest {
     public void JSONToObject() throws Exception
     {
         Division root = new Division();
-        Node dItem = getDivisionNode(PROGRAM_NAME);
+        Node dItem = jsonService.getDivisionNode(PROGRAM_NAME);
         String nextItem = (String) dItem.getProperty(DIVISION_DISPLAY_PROPERTY);
         root.setLabel(nextItem);
         root.setId(dItem.getId());
-        buildJSON(dItem, root);
-        String temp = mapper.defaultPrettyPrintingWriter().writeValueAsString(root);
-        
-        Division d2 = mapper.readValue(temp, Division.class);
-        
+        jsonService.buildJSON(dItem, root);
+        String temp = jsonService.structureToString(root);
+        Division d2 = jsonService.stringToStructure(temp);
         assertEquals(d2.getChildren().get(2).getLabel(),root.getChildren().get(2).getLabel());
         
         
@@ -78,67 +78,4 @@ public class JsonHospitalTests extends BaseNeo4jTest {
         
     }
 
-    private void buildJSON(Node item, Division parent) {
-        String nextItem = getDisplayMessage(item);
-        Iterable<Relationship> rels =
-                //  item.getRelationships(Direction.OUTGOING,
-                //  RelationshipTypes.IS_DIVIDED_INTO);
-
-                item.getRelationships(Direction.OUTGOING);
-        parent.setLabel(nextItem);
-
-        if (rels.iterator().hasNext()) {
-            for (Relationship r : rels) {
-                Division div = new Division();
-                Node currentNode = r.getEndNode();
-                String lVar = getDisplayMessage(currentNode);
-                div.setLabel(lVar);
-                div.setId(currentNode.getId());
-                parent.getChildren().add(div);
-                buildJSON(currentNode, div);
-            }
-
-
-        }
-
-
-
-
-    }
-
-    private String getDisplayMessage(Node currentNode) {
-        NODE_TYPE type = getNodeType(currentNode);
-        String lVar = "";
-        switch (type) {
-            case DIVISIONS:
-
-                lVar = (String) currentNode.getProperty(DIVISION_DISPLAY_PROPERTY);
-                break;
-
-            case PROVIDERS:
-                lVar = (String) currentNode.getProperty(PROVIDER_DISPLAY_PROPERTY);
-                break;
-
-            default:
-                throw new RuntimeException(" got fault " + currentNode.toString());
-        }
-        return lVar;
-    }
-
-    private NODE_TYPE getNodeType(Node node) {
-        String t = (String) node.getProperty(NODE_TYPE.TYPE.toString());
-        NODE_TYPE res = NODE_TYPE.valueOf(t);
-        if (res == null) {
-            throw new RuntimeException("got node type error " + t);
-        }
-        return res;
-    }
-
-    private Node getDivisionNode(String nodeName) {
-        Index<Node> indexDivisionsDisplay =
-                staticgraphDb.index().forNodes(DIVISION_DISPLAY_INDEX);
-        Node dItem =
-                indexDivisionsDisplay.get(DIVISION_DISPLAY_PROPERTY, nodeName).getSingle();
-        return dItem;
-    }
 }
